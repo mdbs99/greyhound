fa2a99a3e891dee4fb04ab78d6cb71a9a24c8b36
Marcos Douglas <mdbs99@delfire.net>
2012-11-21 17:34:57 -0300

    Updated mssql/t1 example

--- examples/sql/mssql/t1.pas
+++ examples/sql/mssql/t1.pas
@@ -8,110 +8,106 @@ uses
   // gh
   gh_SQL, gh_SQLdbLib;
 
-{$DEFINE MSSQLBroker}
-
 const
-  TABLE_NAME = '#user_tmp';
+  TABLE_NAME = 'user_tmp001';
 
 var
-  co: TghSQLConnector;
-  sql: TghSQLObject;
+  Co: TghSQLConnector;
+  User: TghSQLTable;
+  SQL: TghSQLObject;
 
-procedure ExecSelect;
+procedure ShowUser;
 var
-  ds: TDataSet;
+  lStr: string;
 begin
-  writeln;
-  writeln('Show data:');
-
-  sql.Clear;
-  sql.Script.Text := 'select * from ' + TABLE_NAME;
-  sql.Open(ds);
-  try
-    while not ds.EOF do
-    begin
-      writeln('User: ', ds.FieldByName('id').AsString, '-', ds.FieldByName('name').AsString);
-      ds.Next;
-    end;
-  finally
-    ds.Free;
-  end;
+  lStr := Format('#%d %s(%s)', [User['id'].AsInteger, User['name'].AsString, User['passwd'].AsString]);
+  writeln(lStr);
 end;
 
-procedure InsertUser(const ALogin, APasswd, AName: string);
+procedure ShowAll;
 begin
   writeln;
-  writeln('Inserting ', ALogin, ' ', AName);
-
-  with sql do
+  writeln('Show all:');
+  if not User.IsEmpty then
   begin
-    Clear;
-    Prepared := True;
-    Script.Text := 'insert into ' + TABLE_NAME + ' values (:login, :passwd, :name)';
-    Params['login'].AsString := ALogin;
-    Params['passwd'].AsString := APasswd;
-    Params['name'].AsString := AName;
-    Execute;
-  end;
+    User.First;
+    while not User.EOF do
+    begin
+      ShowUser;
+      User.Next;
+    end;
+  end
+  else
+    writeln('No records found.');
+
+  writeln;
 end;
 
 begin
-  co := TghSQLConnector.Create;
-  sql := TghSQLObject.Create(co);
+  Co := TghSQLConnector.Create;
+  SQL := TghSQLObject.Create(Co);
   try
     // set configurations
     // using MSSQLServer
-    co.SetLibClass(TghMSSQLLib);
+    Co.SetLibClass(TghMSSQLLib);
 
     // set params
-    co.Host := 'YOUR_HOST';
-    co.Database := 'YOUR_DATABASE';
-    co.User := 'YOUR_USER';
-    co.Password := 'YOUR_PASSWORD';
+    Co.Host := 'HOST';
+    Co.Database := 'DATABASE';
+    Co.User := 'USER';
+    Co.Password := 'PASSWORD';
 
-    co.Connect;
+    Co.Connect;
     writeln('Connected.');
 
     // creating a temp table
-    sql.Clear;
-    sql.Script.Add('create table ' + TABLE_NAME + ' ( ');
-    sql.Script.Add('  [id] int identity not null primary key ');
-    sql.Script.Add(' ,[login] varchar(20) not null ');
-    sql.Script.Add(' ,[passwd] varchar(30) null ');
-    sql.Script.Add(' ,[name] varchar(50) null )');
-    sql.Execute;
+    SQL.Clear;
+    SQL.IsBatch := True;
+    SQL.Script.Add('if object_id ('''+TABLE_NAME+''') is not null');
+    SQL.Script.Add('  drop table '+TABLE_NAME);
+    SQL.Script.Add('create table '+TABLE_NAME+' ( ');
+    SQL.Script.Add('  [id] int identity not null primary key ');
+    SQL.Script.Add(' ,[login] varchar(20) not null ');
+    SQL.Script.Add(' ,[passwd] varchar(30) null ');
+    SQL.Script.Add(' ,[name] varchar(50) null )');
+    SQL.Execute;
     writeln('Table created.');
 
-    // insert
-    InsertUser('mjane', '123', 'Mary Jane');
+    User := Co.Tables[TABLE_NAME].Open;
 
-    ExecSelect;
+    User.Append;
+    User['login'].AsString := 'mjane';
+    User['passwd'].AsString := '123';
+    User['name'].AsString := 'Mary Jane';
+    User.Commit;
 
-    // using transaction
-    co.StartTransaction;
-    InsertUser('venon', 'xxx', 'Venon');
-    ExecSelect;
-    writeln('Rollback');
-    co.Rollback;
+    ShowAll;
 
-    ExecSelect;
+    User.Append;
+    User['login'].AsString := 'venon';
+    User['passwd'].AsString := 'v12$';
+    User['name'].AsString := 'Venon';
+    User.Post;
 
-    // again, using Commit
-    co.StartTransaction;
-    InsertUser('pparker', '1', 'Peter Parker');
-    writeln('Commit');
-    co.Commit;
+    User.Append;
+    User['login'].AsString := 'pparker';
+    User['passwd'].AsString := 'pp123';
+    User['name'].AsString := 'Peter Parker';
+    User.Post;
 
-    ExecSelect;
+    User.Commit;
+
+    // see the id values (autoinc)
+    ShowAll;
 
     // drop table
-    sql.Clear;
-    sql.Script.Text := 'drop table '+TABLE_NAME;
-    sql.Execute;
+    SQL.Clear;
+    SQL.Script.Text := 'drop table '+TABLE_NAME;
+    SQL.Execute;
 
   finally
-    sql.Free;
-    co.Free;
+    SQL.Free;
+    Co.Free;
   end;
 
   writeln;
