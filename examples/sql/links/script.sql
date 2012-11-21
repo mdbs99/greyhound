3714b9249b716d28864b1779697be00bcc275f9f
Marcos Douglas <mdbs99@delfire.net>
2012-11-21 11:47:29 -0300

    Clean up

--- examples/sql/links/script.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS "access";
-CREATE TABLE "access" ("id" int, "name" varchar(40));
-INSERT INTO "access" VALUES(1,'admin');
-INSERT INTO "access" VALUES(2,'default');
-DROP TABLE IF EXISTS "user";
-CREATE TABLE user (id int, login varchar(30), "access_id" int);
-INSERT INTO "user" VALUES(1,'bob',2);
-INSERT INTO "user" VALUES(2,'paul',1);
